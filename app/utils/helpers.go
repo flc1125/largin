4f05dc809254cec96b900c78a713acd870a1b1a3
Flc <four_leaf_clover@foxmail.com>
2021-01-15 09:56:48 +0800

    route

--- app/utils/helpers.go
+++ app/utils/helpers.go
@@ -1,6 +1,8 @@
 package utils
 
-import "os"
+import (
+	"os"
+)
 
 // Env 读取环境变量，若
 func Env(key string, d string) string {
--- bootstrap/application.go
+++ bootstrap/application.go
@@ -8,12 +8,12 @@ import (
 func Application() *gin.Engine {
 	e := gin.Default()
 
-	// 服务，注意先后顺序（如：模板、中间件、路由）
+	// 服务管理，注意先后顺序
 	providers.Env(e)        // 环境配置
 	providers.Config(e)     // 系统配置
 	providers.App(e)        // 全局应用服务
 	providers.Cache(e)      // 缓存
-	providers.View(e)       // 模板
+	providers.View(e)       // 模板（若无需模板，可注释）
 	providers.Middleware(e) // 中间件
 	providers.Route(e)      // 路由
 
