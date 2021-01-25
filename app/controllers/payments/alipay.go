e5b117ec4ac65d1d8f8ac7ed356dcc300f557ece
Flc <four_leaf_clover@foxmail.com>
2021-01-25 11:15:37 +0800

    test

--- app/controllers/payments/alipay.go
+++ app/controllers/payments/alipay.go
@@ -1,9 +1,17 @@
 package payments
 
-import "github.com/gin-gonic/gin"
+import (
+	"os/exec"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Alipay(c *gin.Context) {
+	cmd := exec.Command("ls", "/")
+	output, _ := cmd.CombinedOutput()
+
 	c.JSON(200, gin.H{
 		"payments": "alipay",
+		"ret":      output,
 	})
 }
